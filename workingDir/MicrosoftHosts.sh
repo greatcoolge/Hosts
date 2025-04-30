f3730f183aa78621fd8b59c5a491787cb011c2dc
greatcoolge <121862222+greatcoolge@users.noreply.github.com>
2025-05-01 06:18:18 +0800

    MicrosoftHosts.sh

--- workingDir/MicrosoftHosts.sh
+++ workingDir/MicrosoftHosts.sh
@@ -1,1 +1,1 @@
-python ph.py -f SteamDomains GithubDomains EpicDomains MicrosoftDomains -o ../hosts.txt --domain-out ../Microsoftdomains.txt --ip-out ../Microsoftips.txt
+python ph.py -f MicrosoftDomains -o ../hosts.txt --domain-out ../Microsoftdomains.txt --ip-out ../Microsoftips.txt
